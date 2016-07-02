9ff2b6b706f5fb6c93c86ba1b489a9c7e7e87485
Rajesh Manikka <raj@indix.com>
2016-07-02 23:32:00 +0530

    firstcut solution for timeseries

--- src/main/scala/timeseries/TimeSeries.scala
+++ src/main/scala/timeseries/TimeSeries.scala
@@ -2,32 +2,49 @@ package timeseries
 
 import scala.io.Source
 
-
 object TimeSeries {
-  case class Event(timeStamp:Long, priceRatio:Double, eventsCount:Int = 0,
-                   rollingSum:Double=0, minPriceRatio:Double=0, maxPriceRatio:Double=0){
-    def header = "T\tV\tN\tRS\tMinV\tMaxV"
 
+  val WINDOW = 60
+
+  case class Event(timeStamp:Long, priceRatio:Double, eventsCount:Int,
+                   rollingSum:Double, minPriceRatio:Double, maxPriceRatio:Double){
+    var prev:Option[Event] = None
+    def header = "T\tV\tN\tRS\tMinV\tMaxV"
     override def toString = s"$timeStamp\t$priceRatio\t$eventsCount\t$rollingSum\t$minPriceRatio\t$maxPriceRatio"
+    def ++(that:Event) = {
+      val e = Event(this.timeStamp, this.priceRatio,
+        this.eventsCount + that.eventsCount,
+        this.rollingSum + that.priceRatio,
+        Seq(this.minPriceRatio, that.priceRatio).min,
+        Seq(this.maxPriceRatio, that.priceRatio).max
+      )
+      e.prev = that.prev
+      e
+    }
   }
-  case class Chain(left:Option[Event], right:Option[Event])
 
   def toEvent(string:String) = {
     val splits = string.split("[ \t]")
-    Event(splits(0).toLong, splits(1).toDouble)
+    val timestamp = splits(0).toLong
+    val priceRatio = splits(1).toDouble
+    Event(timestamp, priceRatio, 1, priceRatio, priceRatio, priceRatio)
   }
 
-  def toChain(events:Stream[Event]) = Chain(None,Option(events.head)) #:: events.zip(events.tail)
-    .map{ case (prev, current) => Chain(Option(prev), Option(current))}
+  def toChain(events:Stream[Event]) = events.head #:: events.zip(events.tail).map{
+      case (prev, current) => current.prev = Option(prev); current
+  }
+
+  def aggregate(current:Event):Event = current.prev match {
+    case Some(prev) if current.timeStamp - prev.timeStamp < WINDOW => aggregate(current ++ prev)
+    case _ => current
+  }
 
   def main(args: Array[String]) {
     val events = Source.fromFile("/Users/raj/workspace/scala-de-challenge/src/main/resources/data_scala.txt").getLines()
       .map(toEvent)
-      .toStream
 
-    val chainedEvents = toChain(events)
-
-
-
+    val chainedEvents = toChain(events.toStream)
+      .map(aggregate)
+      .foreach(p => println(p))
   }
 }
