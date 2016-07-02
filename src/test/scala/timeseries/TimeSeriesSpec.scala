d9b7f9945a6e8fbbe9e5b853459b0ff7d355c4a8
Rajesh Manikka <raj@indix.com>
2016-07-02 23:31:25 +0530

    adding spec for timeseries challenge

--- src/test/scala/timeseries/TimeSeriesSpec.scala
+++ src/test/scala/timeseries/TimeSeriesSpec.scala
@@ -1,25 +1,15 @@
 package timeseries
 
 import org.scalatest.{Matchers, FlatSpec}
-import timeseries.TimeSeries.{Chain, Event}
+import timeseries.TimeSeries
+import timeseries.TimeSeries.Event
 
 
-class TimeSeriesSpec extends FlatSpec with Matchers{
+class TimeSeriesSpec extends FlatSpec with Matchers {
 
   "TimeSeries" should "convert tab/space separated string to event" in {
-    TimeSeries.toEvent("1\t1.0") should be(Event(1, 1.0))
-    TimeSeries.toEvent("1 1.0") should be(Event(1, 1.0))
+    TimeSeries.toEvent("1\t1.0") should be(Event(1, 1.0, 1, 1.0, 1.0, 1.0))
+    TimeSeries.toEvent("1 1.0") should be(Event(1, 1.0, 1, 1.0, 1.0, 1.0))
   }
-
-  it should "transform event stream to event chain" in {
-    val events = Stream(Event(1,1.0), Event(2,2.0), Event(3,3.0))
-    val expected = Stream(Chain(None, Some(Event(1,1.0))),
-      Chain(Some(Event(1,1.0)), Some(Event(2,2.0))),
-      Chain(Some(Event(2,2.0)), Some(Event(3,3.0)))
-    )
-    TimeSeries.toChain(events) should be(expected)
-  }
-
-
 
 }
