925620e91878a1c329208aa9deeb6b4ea33c1903
Rajesh Manikka <raj@indix.com>
2016-07-03 14:06:51 +0530

    refactored and added tests

--- src/main/scala/timeseries/TimeSeries.scala
+++ src/main/scala/timeseries/TimeSeries.scala
@@ -1,36 +1,51 @@
 package timeseries
 
 import scala.io.Source
+import Implicits._
 
-object TimeSeries {
-
-  val WINDOW = 60
-  def header = {
-    println(s"T${" " * 10}V${" "*7}N${" " * 1}RS${" "*6}MinV${" "*4}MaxV")
-    println("-" * 45)
+object Implicits{
+  case class Precision(p:Double)
+  implicit class DoubleWithAlmostEquals(val d:Double) extends AnyVal {
+    def ~=(d2:Double)(implicit p:Precision) = (d - d2).abs < p.p
   }
+}
 
-  case class Event(timeStamp:Long, priceRatio:Double, eventsCount:Int,
-                   rollingSum:Double, minPriceRatio:Double, maxPriceRatio:Double){
-    var previous:Option[Event] = None
-    override def toString = f"$timeStamp $priceRatio%.5f $eventsCount $rollingSum%.5f $minPriceRatio%.5f $maxPriceRatio%.5f"
-    def ++(that:Event) = {
-      val aggregatedEvent = Event(this.timeStamp, this.priceRatio,
-        this.eventsCount + that.eventsCount,
-        this.rollingSum + that.priceRatio,
-        Seq(this.minPriceRatio, that.priceRatio).min,
-        Seq(this.maxPriceRatio, that.priceRatio).max
-      )
-      aggregatedEvent.previous = that.previous
-      aggregatedEvent
-    }
+case class Event(timeStamp:Long, priceRatio:Double, eventsCount:Int,
+                 rollingSum:Double, minPriceRatio:Double, maxPriceRatio:Double){
+  implicit val precision = Precision(0.00001)
+  var previous:Option[Event] = None
+  def ++(that:Event) = {
+    val aggregatedEvent = Event(this.timeStamp, this.priceRatio,
+      this.eventsCount + that.eventsCount,
+      this.rollingSum + that.priceRatio,
+      Seq(this.minPriceRatio, that.priceRatio).min,
+      Seq(this.maxPriceRatio, that.priceRatio).max
+    )
+    aggregatedEvent.previous = that.previous
+    aggregatedEvent
   }
+  override def toString = f"$timeStamp $priceRatio%.5f $eventsCount $rollingSum%.5f $minPriceRatio%.5f $maxPriceRatio%.5f"
+  override def equals(any:Any) = any match {
+    case that:Event => Seq(this.timeStamp==that.timeStamp, this.priceRatio ~= that.priceRatio, this.eventsCount == that.eventsCount,
+    this.rollingSum ~= that.rollingSum, this.minPriceRatio ~= that.minPriceRatio, this.maxPriceRatio ~= that.maxPriceRatio).reduce(_ && _)
+    case _ => false
+  }
+}
 
-  def toEvent(string:String) = {
+object Event{
+  def apply(string:String):Event = {
     val splits = string.split("[ \t]")
     val timestamp = splits(0).toLong
     val priceRatio = splits(1).toDouble
     Event(timestamp, priceRatio, 1, priceRatio, priceRatio, priceRatio)
+  }
+}
+
+object TimeSeries {
+  val WINDOW = 60
+  def printHeader(){
+    println(s"T${" " * 10}V${" "*7}N${" " * 1}RS${" "*6}MinV${" "*4}MaxV")
+    println("-" * 45)
   }
 
   def toChain(events:Stream[Event]) = events.head #:: events.zip(events.tail).map{
@@ -42,13 +57,17 @@ object TimeSeries {
     case _ => current
   }
 
-  def main(args: Array[String]) {
-    header
-    val events = Source.fromFile("/Users/raj/workspace/scala-de-challenge/src/main/resources/data_scala.txt").getLines()
-      .map(toEvent)
+  def apply(file:String) = {
+    val events = Source.fromFile(file).getLines().map(s => Event(s))
+    toChain(events.toStream).map(aggregate)
+  }
 
-    toChain(events.toStream)
-      .map(aggregate)
-      .foreach(p => println(p))
+  def main(args: Array[String]) {
+    if(args.length < 1){
+      println("Usage: timeseries.TimeSeries <input_file>")
+      System.exit(0)
+    }
+    printHeader()
+    TimeSeries(args(0)).foreach(p => println(p))
   }
 }
--- src/test/scala/timeseries/TimeSeriesSpec.scala
+++ src/test/scala/timeseries/TimeSeriesSpec.scala
@@ -1,15 +1,51 @@
 package timeseries
 
-import org.scalatest.{Matchers, FlatSpec}
-import timeseries.TimeSeries
-import timeseries.TimeSeries.Event
+import org.scalatest.{FlatSpec, Matchers}
 
 
 class TimeSeriesSpec extends FlatSpec with Matchers {
 
   "TimeSeries" should "convert tab/space separated string to event" in {
-    TimeSeries.toEvent("1\t1.0") should be(Event(1, 1.0, 1, 1.0, 1.0, 1.0))
-    TimeSeries.toEvent("1 1.0") should be(Event(1, 1.0, 1, 1.0, 1.0, 1.0))
+    Event("1\t1.0") should be(Event(1, 1.0, 1, 1.0, 1.0, 1.0))
+    Event("1 1.0") should be(Event(1, 1.0, 1, 1.0, 1.0, 1.0))
   }
+
+  it should "chain events" in {
+    val e1 = Event(1, 1.0, 1, 1.0, 1.0, 1.0)
+    val e2 = Event(2, 2.0, 2, 2.0, 2.0, 2.0)
+    val e3 = Event(3, 3.0, 3, 3.0, 3.0, 3.0)
+    val chained = TimeSeries.toChain(Stream(e1, e2, e3))
+    chained.head.previous should be(None)
+    chained(1).previous should be (Some(e1))
+    chained(2).previous should be (Some(e2))
+  }
+
+  it should "aggregate two events using ++ def" in {
+    val previous = Event(1, 1.0, 1, 1.0, 1.0, 1.0)
+    val current = Event(2, 2.0, 2, 2.0, 2.0, 2.0)
+    val expected = Event(2, 2.0, 3, 3.0, 1.0, 2.0)
+    expected.previous = Some(previous)
+    current ++ previous should be(expected)
+  }
+
+  it should "aggregate based on window" in {
+    val e1 = Event(10, 1.0, 1, 1.0, 1.0, 1.0)
+    val e2 = Event(21, 2.0, 2, 2.0, 2.0, 2.0)
+    val e3 = Event(3, 3.0, 3, 3.0, 3.0, 3.0)
+  }
+
+  it should "return a stream give a input text file" in {
+    val stream = TimeSeries(this.getClass.getResource("/test_input.txt").getPath)
+    stream.head should be(Event(1355270609, 1.80215, 1, 1.80215, 1.80215, 1.80215))
+    stream(1) should be(Event(1355270621,	1.80185,	2,	3.604,	1.80185,	1.80215))
+    stream(2) should be(Event(1355270646,	1.80195,	3,	5.40595,	1.80185,	1.80215))
+    stream(3) should be(Event(1355270702,	1.80225,	2,	3.6042,	1.80195,	1.80225))
+    stream(4) should be(Event(1355270702,	1.80215,	3,	5.40635,	1.80195,	1.80225))
+    stream(5) should be(Event(1355270829,	1.80235,	1,	1.80235,	1.80235,	1.80235))
+  }
+
+
+
+
 
 }
